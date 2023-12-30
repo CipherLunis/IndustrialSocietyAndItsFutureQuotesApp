6cc1ea33596415443c80768ca54cd38f754d991a
Cipher Lunis <cipheryxinquiries@gmail.com>
2023-12-30 13:23:59 -0600

    Initial Commit

---
+++ IndustrialSocietyAndItsFutureQuotes/IndustrialSocietyAndItsFutureQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndustrialSocietyAndItsFutureQuotesApp.swift
+//  IndustrialSocietyAndItsFutureQuotes
+//
+//  Created by Cipher Lunis on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct IndustrialSocietyAndItsFutureQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
